38c2430686ecae67c8d02dec32557005a801d5df
hitsu0905 <hitsu0905@gmail.com>
2025-04-21 20:23:41 +0900

    get image plus

--- pov_demo_app/src/hooks/useImage.js
+++ pov_demo_app/src/hooks/useImage.js
@@ -1,11 +1,30 @@
 import { createLogMessage } from '../utils/logUtils';
 
 export const useImage = (addLog) => {
+  let video = null;
+
   const handleImageClick = () => {
     const message = createLogMessage('Image button clicked');
     addLog(message);
     console.log('Capturing image from webcam');
-  };
 
-  return { handleImageClick };
+    video = document.createElement('video');
+    video.setAttribute('autoplay', true);
+    video.setAttribute('playsinline', true);
+    video.style.display = 'none';
+    document.body.appendChild(video);
+
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+      .then((stream) => {
+        video.srcObject = stream;
+        console.log('カメラアクセス成功！');
+      }
+    )
+      .catch((err) => {
+        console.error('カメラアクセスエラー:', err);
+      }
+    );
+  };
+  const getVideoElement = () => video;
+  return { handleImageClick, getVideoElement };
 };
