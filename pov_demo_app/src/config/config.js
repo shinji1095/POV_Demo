dafd64af0dd0451064c624cba78241218b3ce5f2
*NgVSQlksmH <shinji.eto@maris-inc.co.jp>
2025-04-25 22:06:08 +0900

    add: 振動時間，振動間隔，音声の再生速度と再生間隔を調整

--- pov_demo_app/src/config/config.js
+++ pov_demo_app/src/config/config.js
@@ -1,10 +1,10 @@
 export const config = {
     // 音声関係
-    AUDIO_INTERVAL: 300, 
-    AUDIO_PATH: '/sound/good_highlow_1_speed_3.mp3',
+    AUDIO_INTERVAL: 1, 
+    AUDIO_PATH: '/sound/bad_highlow_2_speed_1.mp3',
 
     // 振動関係
-    VIBRATION_DURATION: 200,  // 振動時間（ms）
+    VIBRATION_DURATION: 500,  // 振動時間（ms）
     VIBRATION_INTERVAL: 1000, // 振動+停止の合計周期（ms）
 
     // IMU
